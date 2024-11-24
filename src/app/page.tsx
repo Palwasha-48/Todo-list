0d3c34c75bdd79ac87fd9d3a72da1b9f25836c29
Palwasha-48 <163187583+Palwasha-48@users.noreply.github.com>
2024-11-24 14:10:57 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,26 @@
 "use client"
-
-import Image from "next/image";
-
-import {useState} from 'react'
-
+import  React, {useState, useEffect} from 'react'
 const page = () => {
     const[task,settask] = useState("")
-    const[addnewtask,setaddnewtask] = useState("")
     const[maintask,setmaintask] = useState([])
-
-const submitHandler = (e: { preventDefault: () => void; }) => {
-    e.preventDefault()
+const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+     e.preventDefault(); 
     setmaintask([...maintask,{task}])
-
     console.log(maintask)
-
     settask("")
 } 
-
-const deleteHandler = (i: string) => {
- let copytask = [...maintask]
+const deleteHandler = (i: number) => {
+ const copytask = [...maintask]
  copytask.splice(i,1)
  setmaintask(copytask)
 }
 
-let rendertask = <h2></h2>
 
-
-    rendertask = maintask.map((t, i) =>{
-    return(
+const rendertask = maintask.map((t, i) =>(
         <li key={i}>
         <div>
             <h3>{t.task}</h3>
         </div>
-
         <button onClick = {() => {
             deleteHandler(i)
         }}
@@ -42,45 +29,34 @@ let rendertask = <h2></h2>
         </button>
         </li>
     )
-})
-
-  return (
+    return (
     <>
         <div className= "text-6xl pt-20 font-serif text-center text-pink-950">
             TO-DO LIST
         </div>
-
         <div className="pt-8">
             <form onSubmit={submitHandler}>
-            <input type="text" placeholder="Add a new task" value={task} onChange={(e) => { settask(e.target.value)
-                   }}
+            <input type="text" placeholder="Add a new task" value={task} onChange={(e) => settask((prevTask) => e.target.value)
+                   }
                    className= "pl-4 rounded-xl bg-pink-200 w-96 h-12 "/>
             </form>
-
         </div>
-
         <div className= "pt-8"> 
             <button 
-            onClick={() =>{
-                submitHandler(e)
-            }}
+            onClick={submitHandler}
             className= "bg-amber-800 hover:bg-amber-600 rounded-full w-28 h-12 text-2xl font-serif shadow-2xl">
                 Click
             </button>
         </div>
-
         <div className="text-amber-900 pt-10 font-serif text-4xl text-left pl-72">
             <ul>
                 {rendertask}
             </ul>
         </div>  
-
-        <footer className="pb-9 pt-9 align-text-bottom text-pink-950 align-text-bottom">
+        <footer className="pb-9 pt-9 align-text-bottom text-pink-950">
             Created by PALWASHA
         </footer>
     </>
   )
 }
-
 export default page
-
